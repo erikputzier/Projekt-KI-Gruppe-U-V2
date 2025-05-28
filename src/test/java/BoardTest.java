5bcf481626367a780f74397864893d50d092214a
ep <ep@trustami.com>
2025-05-28 16:09:19 +0200

    went through some more editor suggestions

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -17,11 +17,11 @@ public class BoardTest {
     @Test
     public void numPiecesTest() {
         Board board = new Board("7/7/3r1BG2/4r1RG1/7/7/7 r");
-        assertEquals(board.numPieces(Player.RED), 3);
-        assertEquals(board.numPieces(Player.BLUE), 1);
+        assertEquals(3, board.numPieces(Player.RED));
+        assertEquals(1, board.numPieces(Player.BLUE));
 
         board = new Board("r1r11RG1r1r1/2r11r12/3r13/7/3b13/2b11b12/b1b11BG1b1b1 r");
-        assertEquals(board.numPieces(Player.BLUE), 8);
-        assertEquals(board.numPieces(Player.RED), 8);
+        assertEquals(8, board.numPieces(Player.BLUE));
+        assertEquals(8, board.numPieces(Player.RED));
     }
 }
--- src/test/java/MinimaxTest.java
+++ src/test/java/MinimaxTest.java
@@ -1,16 +1,12 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 import java.util.concurrent.atomic.AtomicInteger;
-
 
 public class MinimaxTest {
 
     @Test
     public void minimaxTest() {
         Board board = new Board("3r13/2b12r21/b16/2RG1b22/b16/2b22BG1/3r23 r");
-        BitBoardUtils utils = new BitBoardUtils();
         AtomicInteger stateCounter = new AtomicInteger();
         long start = System.currentTimeMillis();
         System.out.println(BitBoardUtils.minimax(board, 3, false));
@@ -23,8 +19,6 @@ public class MinimaxTest {
     @Test
     public void minimaxAlphaBetaTest() {
         Board board = new Board("3RG3/7/7/7/4b11b1/4r4r11/3BG1b11 b");
-        BitBoardUtils utils = new BitBoardUtils();
-
-        System.out.println(utils.minimaxAlphaBeta(board, 1000));
+        System.out.println(BitBoardUtils.minimaxAlphaBeta(board, 1000));
     }
 }
